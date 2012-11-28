45aeb49d7349e824d3ba59a39ff74fc87fab25e0
Alexandre Dulaunoy <a@foo.be>
2012-11-28 09:46:29 +0100

    Converted code to Python 3.3
    Tested on Python 3.3

--- src/header.py
+++ src/header.py
@@ -1,5 +1,5 @@
 
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 import sys
 import os
 
@@ -13,14 +13,14 @@ def getServerStatus():
     host = os.environ.get("host", "127.0.0.1")
     port = 28017
     url = "http://%s:%d/_status" % (host, port)
-    req = urllib2.Request(url)
+    req = urllib.request.Request(url)
     user = os.environ.get("user")
     password = os.environ.get("password")
     if user and password:
-        passwdmngr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        passwdmngr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
         passwdmngr.add_password(None, 'http://%s:%d' % (host, port), user, password)
-        authhandler = urllib2.HTTPDigestAuthHandler(passwdmngr)
-        opener = urllib2.build_opener(authhandler)
-        urllib2.install_opener(opener)
-    raw = urllib2.urlopen(req).read()
-    return json.loads( raw )["serverStatus"]
+        authhandler = urllib.request.HTTPDigestAuthHandler(passwdmngr)
+        opener = urllib.request.build_opener(authhandler)
+        urllib.request.install_opener(opener)
+    raw = urllib.request.urlopen(req).read()
+    return json.loads( raw.decode('utf-8') )["serverStatus"]
